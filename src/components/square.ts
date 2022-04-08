c4685ef333ebc5ef8301507c87c4c368e0742bfe
rlagudals95 <ahovmf@naver.com>
2022-04-08 18:41:00 +0900

    github page

--- src/components/square.ts
+++
@@ -1,9 +0,0 @@
-import { BaseComponent } from "./component";
-
-
-export class SquareComponent<T extends HTMLElement> extends BaseComponent<T> {
-
-    constructor(canvas: HTMLElement, htmlString: string, x: number, y: number, cnt: any) {
-        super(canvas, htmlString, x, y, cnt);
-    }
-}
